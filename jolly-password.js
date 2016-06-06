c6c83bfab161e1000ba4e31f84396fa62c2ae16c
Jonathan Tavares <jtavares.codes@gmail.com>
2016-06-06 22:20:38 +0000

    adds jolly-password.js

---
+++ jolly-password.js
@@ -0,0 +1,75 @@
+'use strict';
+
+function _normalize(min, max, num){
+  if(num<=min) return min;
+  if(num>=max) return max;
+  if(num==69) return _normalize(min+(Math.random()*max))
+  return parseInt(num);
+}
+
+function _cardinalNumber(num){
+  let _number = _normalize(1,99,num);
+  
+  switch (_number) {
+    case 1: return 'one';
+    case 2: return 'two';
+    case 3: return 'three';
+    case 4: return 'four';
+    case 5: return 'five';
+    case 6: return 'six';
+    case 7: return 'seven';
+    case 8: return 'eight';
+    case 9: return 'nine';
+    case 10: return 'ten';
+    case 11: return 'eleven';
+    case 12: return 'twelve';
+    case 13: return 'thirteen';
+    case 14: return 'fourteen';
+    case 15: return 'fifteen';
+    case 16: return 'sixteen';
+    case 17: return 'seventeen';
+    case 18: return 'eighteen';
+    case 19: return 'nineteen';
+    case 20: return 'twentie';
+    case 30: return 'thirty';
+    case 40: return 'forty';
+    case 50: return 'fifty';
+    case 60: return 'sixty';
+    case 70: return 'seventy';
+    case 80: return 'eighty';
+    case 90: return 'ninety';
+    default:
+      if(_number>21){
+        let d = _number.toString().split("").shift()*10;
+        let u = _number.toString().split("").pop();
+        return [d,u].map(_cardinalNumber).join(" ")
+      }
+  }
+  
+}
+
+
+function _randLineFromFile(file){
+  const words = require('fs').readFileSync(file).toString().split("\n")
+  const rand = Math.round(Math.random()*words.length)
+  const index  = _normalize(0,words.length, rand)
+
+  
+  return words[index];
+}
+
+
+function generate() {
+  return  [
+      _cardinalNumber(Math.random()*100),
+      _randLineFromFile('./data/words.txt'),
+      _randLineFromFile('./data/animals.txt') + "s"
+      ].join(" ");
+}
+
+module.exports = {
+  _randLineFromFile,
+  _cardinalNumber,
+  _normalize,
+  generate
+}
---
+++ test.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const assert = require('assert');
+
+
+describe("test runner", () => {
+    it("should run tests", () => {
+        assert.ok(true);
+    });
+});
+describe("JollyPassword", () => {
+    
+    let JollyPassword = require("./jolly-password").generate;
+    
+    it("should generate random password", () => {
+        assert.ok(JollyPassword());
+    });
+});
+
+describe("_normalize", () => {
+    
+    let _normalize = require("./jolly-password")._normalize;
+    
+    it("should return min>num<max", () => {
+        assert.equal(_normalize(0,10,0),0);
+        assert.equal(_normalize(0,10,-1),0);
+        assert.equal(_normalize(0,10,10),10);
+        assert.equal(_normalize(0,10,11),10);
+        assert.equal(_normalize(0,10,5),5);
+    });
+     it("should ignore 69 by regenerating number", () => {
+        assert.notEqual(_normalize(0,100,69), 69);
+        assert.notEqual(_normalize(0,100,69), 69);
+        assert.notEqual(_normalize(0,100,69), 69);
+        assert.notEqual(_normalize(0,100,69), 69);
+    });
+});
+
+
+describe("_cardinalNumber", () => {
+    
+    let _cardinalNumber = require("./jolly-password")._cardinalNumber;
+    
+    it("should convert int to cardinal number", () => {
+        
+        assert.equal(_cardinalNumber(1),'one');
+        assert.equal(_cardinalNumber(2),'two');
+        
+        assert.equal(_cardinalNumber(22),'twentie two');
+        assert.equal(_cardinalNumber(35),'thirty five');
+        assert.equal(_cardinalNumber(48),'fourty eight');
+    });
+    
+    it("should normalize before convertion", () => {
+        assert.equal(_cardinalNumber(-11),'one'); 
+        assert.equal(_cardinalNumber(123),'ninety nine');
+    });
+    
+    it("should ignore 69 by regenerating number", () => {
+        assert.equal(_cardinalNumber(68),'sixty eight');
+        assert.notEqual(_cardinalNumber(69),'sixty nine');
+        assert.notEqual(_cardinalNumber(69),'sixty nine');
+        assert.notEqual(_cardinalNumber(69),'sixty nine');
+        assert.notEqual(_cardinalNumber(69),'sixty nine');
+        assert.notEqual(_cardinalNumber(69),'sixty nine');
+        assert.equal(_cardinalNumber(70),'seventy');
+    });
+});
+
+describe('_randLineFromFile',() => {
+    
+   var _randLineFromFile = require("./jolly-password")._randLineFromFile;
+    
+    it('get random word', () => {
+        let word = _randLineFromFile('./data/words.txt'); 
+        assert.ok(word)
+    });
+    
+    it('get random words each time', () => {
+    
+        var words = []
+        for(let i=0; i<10; i++){
+            words.push(_randLineFromFile('./data/words.txt'));
+        }
+        
+        words.forEach(function(word, index){
+            assert.ok(words.indexOf(word) === index)
+        });  
+    });
+})
